1cd881fb55d4ef2741919e6bdf8d258458cbc0c6
William <williamcostacardoso94@gmail.com>
2025-12-24 13:16:37 -0300

    feat: adicionando produtos na tabela

--- src/database/seeds/insert-products.ts
+++ src/database/seeds/insert-products.ts
@@ -2,12 +2,19 @@ import { Knex } from "knex";
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
-    await knex("table_name").del();
+    await knex("products").del();
 
     // Inserts seed entries
-    await knex("table_name").insert([
-        { id: 1, colName: "rowValue1" },
-        { id: 2, colName: "rowValue2" },
-        { id: 3, colName: "rowValue3" }
+    await knex("products").insert([
+        { name: "Nhoque quatro queijos", price: 45 },
+        { name: "Isca de frango", price: 60 },
+        { name: "Tilápia com alcaparras", price: 100 },
+        { name: "Escondidinho de carne", price: 75 },
+        { name: "Porção de batatas fritas", price: 65 },
+        { name: "Executivo de frango grelhado", price: 40 },
+        { name: "Executivo de tilápia grelhada", price: 39 },
+        { name: "Caldo de palmito", price: 30 },
+        { name: "Refrigerante 350ml", price: 7.5 },
+        { name: "Suco de laranja 440ml", price: 10 },
     ]);
 };
