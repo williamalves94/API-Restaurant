b9a0661e17c0a3ac7cfd0a006ca3da10718bc31d
William <williamcostacardoso94@gmail.com>
2025-12-25 12:24:20 -0300

    feat: criando a rota das mesas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express"
 
 import { productsRoutes } from "./products-routes"
+import { tablesRoutes } from "./tables-routes"
 
 const routes = Router()
 
 routes.use("/products", productsRoutes)
+routes.use("/tables", tablesRoutes)
 
 export { routes }
