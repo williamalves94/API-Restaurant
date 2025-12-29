cdb0de4b57145d8e23a4e0ed9b75952b95d2558a
William <williamcostacardoso94@gmail.com>
2025-12-29 15:13:38 -0300

    feat: adicionando a rota table_sessions

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express"
 
 import { productsRoutes } from "./products-routes"
 import { tablesRoutes } from "./tables-routes"
+import { tablesSessionsRoutes } from "./tables-sessions-routes"
 
 const routes = Router()
 
 routes.use("/products", productsRoutes)
 routes.use("/tables", tablesRoutes)
+routes.use("/tables-sessions", tablesSessionsRoutes)
 
 export { routes }
