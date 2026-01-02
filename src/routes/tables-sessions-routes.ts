05c45a454c8a29bb17a3ff298e5a28345202abaf
William <williamcostacardoso94@gmail.com>
2026-01-01 22:28:08 -0300

    feat: criando a tabela orders

--- src/routes/tables-sessions-routes.ts
+++ src/routes/tables-sessions-routes.ts
@@ -5,7 +5,11 @@ import { TablesSessionsController } from "@/controllers/tables-sessions-controll
 const tablesSessionsRoutes = Router()
 const tablesSessionsController = new TablesSessionsController()
 
+//“Express, quando alguém fizer um POST nessa rota,
+//chame automaticamente o método create do controller
+//passando request e response.”
 tablesSessionsRoutes.post("/", tablesSessionsController.create)
+// dentro da classe TablesSessionsController pega o index
 tablesSessionsRoutes.get("/", tablesSessionsController.index)
 tablesSessionsRoutes.patch("/:id", tablesSessionsController.update)
 
